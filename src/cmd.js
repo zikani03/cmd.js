faa7b88019a75beafb1bc8a22903d78be0a36038
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2015-05-31 23:41:55 +0200

    Implement Command class as ES2015 class
    Implemented the Command class as an Es2015 class.
    modules exposing `each` type commands should be functional

--- src/cmd.js
+++ src/cmd.js
@@ -1,380 +1,165 @@
+import {add} from './lib/add.js';
+
 /**
- * Command
- * @author Nate Ferrero
+ * Helper to merge array arguments
  */
-(function () {
-    'use strict';
+const merge = function (args) {
+	var _args = [];
+	Array.prototype.forEach.call(args, (arg) => {
+		if (Array.isArray(arg)) {
+			Array.prototype.push.apply(_args, arg);
+		}
+		else {
+			_args.push(arg);
+		}
+	});
+	return _args;
+};
 
-    var scope = this;
-
-    /**
-     * Helper to merge array arguments
-     */
-    var merge = function (args) {
-        var _args = [];
-        Array.prototype.forEach.call(args, function (arg) {
-            if (Array.isArray(arg)) {
-                Array.prototype.push.apply(_args, arg);
-            }
-            else {
-                _args.push(arg);
-            }
-        });
-        return _args;
-    };
-
-    /**
-     * Command class
-     */
-    var Command = function (_fn, name) {
-        this.fn = _fn ? _fn.bind(this) : null;
-        this.name = name;
-    };
-
-    /**
-     * Command.use() loads plugins
-     */
-    Command.prototype.use = function () {
-        var self = this;
-        Array.prototype.forEach.call(arguments, function (name) {
-            if (name === '*') {
-                var libs = [
-                    'add', 'alert',
-                    'call', 'case', 'clone', 'compare', 'count',
-                    'default', 'divide', 'do',
-                    'equals', 'exists', 'extend',
-                    'filter', 'first', 'format',
-                    'get', 'group', 'gt', 'gte',
-                    'has', 'head',
-                    'join',
-                    'last', 'log', 'logger', 'lt', 'lte',
-                    'match', 'max', 'min', 'multiply',
-                    'not',
-                    'obj',
-                    'product', 'push',
-                    'reject', 'reverse',
-                    'slice', 'sort', 'subtract', 'sum',
-                    'tail', 'tap'
-                ];
-                return libs.forEach(function (name) {
-                    self.use(name);
-                });
-            }
-
-            /**
-             * Load via require for Node or from window for browser
-             */
-            var mod = typeof module === 'object' ? require('./lib/' + name) : scope['cmd:lib'][name];
-
-            if (!mod || typeof mod !== 'object') {
-                throw new Error('cmd.js module ' + name + ' is not available');
-            }
-
-            /**
-             * Register the module scope as cmd.name
-             */
-            self.module(name, mod);
-        });
-    };
-
-    /**
-     * Command.module(name, mod) registers a new module as cmd.name
-     */
-    Command.prototype.module = function (name, mod) {
-        if (mod.export) {
-            mod.export(this);
-        }
-        if (mod.all) {
-            this.registerAllFn(name, mod.all, mod);
-        }
-        else if (mod.each) {
-            this.registerEachFn(name, mod.each, mod);
-        }
-        else if (mod.raw) {
-            this.registerRawFn(name, mod.raw);
-        }
-    };
-
-    /**
-     * Command.registerAllFn() causes the function to be called on an array of values
-     * @author Nate Ferrero
-     */
-    Command.prototype.registerAllFn = function (name, fn, plugin) {
-        if (typeof fn !== 'function') {
-            throw new Error('cmd.registerAllFn(name, fn), fn was not a function, got ' + typeof fn);
-        }
-
-        Command.prototype.__defineGetter__(name, function () {
-            var self = this;
-
-            /**
-             * Values loader
-             */
-            var valsLoader = function (args) {
-                var valsFn = self.vals(name, function (vals) {
-                    if (!Array.isArray(vals)) {
-                        vals = [vals];
-                    }
-                    if (self._map) {
-                        return vals.map(function (val) {
-                            if (!Array.isArray(val)) {
-                                val = [val];
-                            }
-                            return fn(args, val);
-                        });
-                    }        
-                    return fn(args, vals);
-                });
-                
-                //console.log(fn.toString())
-                return valsFn;
-            };
-
-            /**
-             * Handle the case where plugin.args is defined
-             */
-            if (typeof plugin.args !== 'undefined') {
-                return valsLoader(plugin.args);
-            }
-
-            /**
-             * Expect the arguments to be provided in the form cmd.x(...args...)(...vals...)
-             * but still allow default argSets to be used
-             */
-            var argsLoader = self.args(name, function (args) {
-                return valsLoader(args);
-            });
-
-            if (typeof plugin.argSets === 'object' && plugin.argSets) {
-                Object.keys(plugin.argSets).forEach(function (key) {
-                    argsLoader.__defineGetter__(key, function () {
-                        return valsLoader(plugin.argSets[key]);
-                    });
-                });
-            }
-            
-            return argsLoader;
-        });
-
-        return cmd[name];
-    };
-
-    /**
-     * Command.registerEachFn() causes the function to be called on each value
-     * @author Nate Ferrero
-     */
-    Command.prototype.registerEachFn = function (name, fn, plugin) {
-        if (typeof fn !== 'function') {
+class Command {
+	
+	constructor (_fn, name) {
+		this.fn = _fn ? _fn.bind(this) : null;
+		this.name = name;
+	}
+	
+	use (module) {
+		const self = this;
+		if (module === '*') {
+			//this.libs.forEach((mod) => this.use(mod));
+            this.module('add', add);
+			return;
+		}
+        // self.module(module, this[module]);
+	}
+	
+	module (name, mod) {
+			
+		if (mod.each) {
+			this.registerEachFn(name, mod.each, mod);
+			return;
+		}
+		
+		if (mod.all) {
+			this.registerAllFn(name, mod.all, mod);
+			return;
+		}
+		
+		if (mod.raw) {
+			this.registerRawFn(name, mod.raw);
+			return;
+		}
+		/*
+		// Early support promises
+		if (mod.then) {
+			this.registerPromiseFn(name, mod.then, mod);
+			return;
+		}
+		*/
+	}
+	
+	get libs() {
+		return [
+			'add'
+		];
+	}
+	
+	registerEachFn (name, fn, module) {
+		if (typeof fn !== 'function') {
             throw new Error('cmd.registerEachFn(name, fn), fn was not a function, got ' + typeof fn);
         }
 
-        Command.prototype.__defineGetter__(name, function () {
-            var self = this;
-
-            /**
-             * Values loader
-             */
-            var valsLoader = function (args) {
-                return self.vals(name, function (vals) {
-                    var eachFn = function (val) {
-                        if (self._map) {
-                            if (!Array.isArray(val)) {
-                                val = [val];
-                            }
-                            return val.map(function (v) {
-                                if (!Array.isArray(v)) {
-                                    v = [v];
-                                }
-                                return fn(args, v);
-                            });
-                        }
-                        
-                        return fn(args, val);
-                    };
-                                        
-
-                    if (!Array.isArray(vals)) {
-                        return eachFn(vals);
-                    }
-                    return vals.map(eachFn);
-                });
-            };
-
-            /**
-             * Handle the case where plugin.args is defined
-             */
-            if (typeof plugin.args !== 'undefined') {
-                return valsLoader(plugin.args);
-            }
-
-            /**
+		// Add a command to the command prototype
+		// Doesn't this affect other commands that may have different 
+		// definitions for the same names?? e.g.
+		// add = each : (args, val) => val + 1 and
+		// add = each : (args, val) => val + args[0]
+		Command.prototype.__defineGetter__(name, function() {
+			const valsLoader = (args) =>
+				this.vals(name, (vals) => {
+					const eachFn = (val) => fn(args, val)
+					
+					if (! Array.isArray(vals)) {
+						return eachFn(vals);
+					}
+					
+					return vals.map(eachFn);
+				});
+			/**
              * Expect the arguments to be provided in the form cmd.x(...args...)(...vals...)
              * but still allow default argSets to be used
              */
-            var argsLoader = self.args(name, function (args) {
-                return valsLoader(args);
-            });
-
-            if (typeof plugin.argSets === 'object' && plugin.argSets) {
-                Object.keys(plugin.argSets).forEach(function (key) {
-                    argsLoader.__defineGetter__(key, function () {
-                        return valsLoader(plugin.argSets[key]);
-                    });
-                });
-            }
-
-            return argsLoader;
-        });
-
-        return cmd[name];
-    };
-
-    /**
-     * Command.registerRawFn() causes to function to be called exactly once with the given arguments
-     */
-    Command.prototype.registerRawFn = function (name, fn) {
-        if (typeof fn !== 'function') {
-            throw new Error('cmd.registerRawFn(name, fn), fn was not a function, got ' + typeof fn);
+            const argsLoader = this.args(name, (args) => valsLoader(args));
+			
+			return argsLoader;
+		});
+		
+	}
+	
+	registerAllFn (name, allFn, module) {
+			
+	}
+	
+	registerRawFn (name, rawFn) {
+		if (typeof rawFn !== 'function') {
+            throw new Error('cmd.registerRawFn(name, fn), fn was not a function, got ' + typeof rawFn);
         }
         Command.prototype.__defineGetter__(name, function () {
-            return fn;
+            return rawFn;
         });
-        return cmd[name];
-    };
-
-    /**
-     * Command.with
-     */
-    Command.prototype.with = function () {
-        if (!this.fn) {
+        return cmd[name];	
+	}
+	
+	get map() {
+		return true;
+	}
+	
+	with () {
+		if (!this.fn) {
             throw new Error('Inappropriate place to call .with()')
         }
-
-        var self = this;
-        var args = Array.prototype.slice.call(arguments);
-        var val = args[0];
-        
-        // Handle promises
-        // TODO: implement a more robust check for whether the argument is a promise or not
-        // TODO: check the other arguments, not just the first!
-        if (val.then) {
-            return val.then(function (resolvedArg) {
-                return self.args('with', self.fn).apply(null, [resolvedArg]);
-            });
-        }
-        /**
-         * Merge multiple subsets of arguments
-         */
-        if (this._map) {
-            return Array.prototype.map.call(arguments, function (args) {
-                if (!Array.isArray(args)) {
-                    args = [args];
-                }
-                return self.args('with', self.fn).apply(self, args);
-            });
-        }
-        
-        return this.args('with', this.fn).apply(null, Array.prototype.slice.call(arguments));
-    };
-
-    /**
-     * Command.to
-     */
-    Command.prototype.to = function () {
-        if (!this.fn) {
-            throw new Error('Inappropriate place to call .to()')
-        }
-        return this.fn(Array.prototype.slice.call(arguments));
-    };
-
-    /**
-     * Command.raw
-     */
-    Command.prototype.__defineGetter__('raw', function () {
-        var self = this;
-        return function (value) {
-            if (!self.fn) {
-                throw new Error('Inappropriate place to call .raw()')
-            }
-            return self.fn(Array.isArray(value) ? value : [value])[0];
-        };
-    });
-
-    /**
-     * Command.map
-     */
-    Command.prototype.__defineGetter__('map', function () {
-        this._map = true;
-        return this;
-    });
-
-    /**
-     * Command.args()
-     * @author Nate Ferrero
-     */
-    Command.prototype.args = function argsWrapper(name, done) {
-        var args = function args() {            
-            return done(merge(arguments));
-        };
-        args.$name = name;
-
-        /**
+		
+		return this.fn(Array.from(arguments));
+		// return this.args('with', this.fn).apply(null, Array.prototype.slice.call(arguments));
+	}
+	
+	to () {
+		if (!this.fn) {
+            throw new Error('fn was not a function, got ' + typeof this.fn);
+		}
+		
+		return this.fn(Array.from(arguments));
+	}
+	
+	args (name, done) {
+		const args = function () {	
+			return done(merge(arguments));
+		};
+		
+		args.$name = name;
+		// TODO: load args here ..
+		
+		/**
          * Skip argument merging with .to()
          */
-        args.__defineGetter__('to', function () {
-            return function unmergedArgs() {
-                var val = arguments[0];
-            
-                if (val.then) {
-                    return val.then(function (resolvedVal) {
-                        return done([resolvedVal]);
-                    });
-                }
-                return done(Array.prototype.slice.call(arguments));
-            };
-        });
+        args.__defineGetter__('to', () =>
+            () => done(Array.from(arguments))
+        );
+		
+		return args;
+	}
+	
+	vals (name, done) {
+		const last = this.fn;
+		
+		return new Command((args)  => {
+			if (last) {
+				args = last(args);
+			}
+			return done(args);
+		}, name);
+	}
+}
 
-        return args;
-    };
 
-    /**
-     * Command.vals()
-     * @author Nate Ferrero
-     */
-    Command.prototype.vals = function valsWrapper(name, done) {
-        var last = this.fn;
-
-        return new Command(function (args) {
-            if (last) {
-                args = last(args);
-            }
-         
-            return done(args);
-        }, name);
-    };
-
-    var cmd = new Command();
-
-    /**
-     * Node.js support
-     */
-    if (typeof module === 'object') {
-        module.exports = cmd;
-    }
-
-    /**
-     * Require.js support
-     */
-    else if (typeof scope.define === 'function') {
-        scope.define([], cmd);
-        scope['cmd:lib'] = {};
-    }
-
-    /**
-     * Otherwise attach to global
-     */
-    else {
-        scope.cmd = cmd;
-        scope['cmd:lib'] = {};
-    }
-}).call(this);
+export var cmd = new Command();
