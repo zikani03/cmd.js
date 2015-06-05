ba1637c527c99b8f7c1dd95e838741fbfd948c0c
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2015-06-05 02:56:17 +0200

    Add gulp-babel as a devDependency

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
+var babel = require('gulp-babel');
 
 var lib = 'src/lib/*.js';
 
@@ -17,5 +18,12 @@ gulp.task('watch', function () {
     gulp.watch(lib, ['lib']);
 });
 
-gulp.task('default', ['lib']);
+// TODO: figure out how to coordinate the build of libs with babel compilation
+gulp.task('babel', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['babel', 'lib']);
 gulp.task('start', ['default', 'watch']);
