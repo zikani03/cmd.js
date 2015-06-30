8e5ab972c1293adb7f66b3a342d48fffab9003e5
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2015-07-01 01:53:02 +0200

    Add babel-base, gulp-babel as dependencies. update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,28 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
+var babel = require('gulp-babel');
 
 var lib = 'src/lib/*.js';
 
 gulp.task('lib', function() {
   return gulp.src(lib)
     .pipe(jshint())
+    .pipe(babel())
     .pipe(concat('cmd.lib.js'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('watch', function () {
     gulp.watch(lib, ['lib']);
 });
 
-gulp.task('default', ['lib']);
+// TODO: figure out how to coordinate the build of libs with babel compilation
+gulp.task('babel', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['babel', 'lib']);
 gulp.task('start', ['default', 'watch']);
